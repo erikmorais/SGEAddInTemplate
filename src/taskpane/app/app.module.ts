1ba6a6c26f6beddf5f4ca91a53511609c7c999dd
erikmorais <erik.pimentel.morais@gmail.com>
2020-06-04 14:50:27 +0100

    nav-menu component

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import AppComponent from "./app.component";
 import WelcomeComponent from "./components/welcome.component";
 import { AppRoutingModule } from './app-routing.module';
+import NaveMenuComponent from './components/nav-menu.component';
 
 
 @NgModule({
-  declarations: [AppComponent,WelcomeComponent],
+  declarations: [AppComponent,WelcomeComponent,NaveMenuComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
